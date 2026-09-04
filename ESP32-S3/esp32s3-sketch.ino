c56436c8258b884a4de42e1016023bc37a07a123
𝑹𝒂𝒇𝒇 𝑨𝒍𝒅𝒔 <117867334+xiv3r@users.noreply.github.com>
2026-09-04 22:56:56 +0800

    Update esp32s3-sketch.ino

--- ESP32-S3/esp32s3-sketch.ino
+++ ESP32-S3/esp32s3-sketch.ino
@@ -2903,6 +2903,7 @@ void setup() {
         timeSource = TIME_SOURCE_NONE;
     }    
     WiFi.mode(WIFI_AP_STA);    
+    WiFi.setTxPower(WIFI_POWER_17dBm);
     if (extConfig.sta_enabled && strlen(sysConfig.sta_ssid) > 0) {
         WiFi.disconnect(true);
         delay(100);
